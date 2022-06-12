7a89a528879f813aa5606490603ea548731e4456
Kengpul <paulpulgares24@gmail.com>
2022-06-12 17:07:08 +0800

    index page

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('view enging', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.listen(3000, () => {
     console.log('on port 3000');
